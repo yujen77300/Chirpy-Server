2d881581b7f71a4c58aee2ac6f299e56cf77e3a3
yujen77300 <yujenhuang24@gmail.com>
2025-03-03 22:42:06 +0800

    feat: add metrics and reset handlers to track and reset file server hits

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 
+	cfg := &apiConfig{}
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
 	mux.Handle("/app/assets/", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
 
@@ -18,6 +24,9 @@ func main() {
 		w.Header().Add("Content-Type", "text/plain")
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("/metrics", cfg.metricsHandler)
+	mux.HandleFunc("/reset", cfg.resetHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -28,4 +37,25 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Println("Server failed:", err)
 	}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "text/plain")
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "text/plain")
+	w.Write([]byte("Hits reset to 0"))
 }
