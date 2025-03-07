1eb281e80b2a95d9cdce58f112d1a674256bbfd9
yujen77300 <yujenhuang24@gmail.com>
2025-03-07 23:53:53 +0800

    feat: profane some words

--- validate.go
+++ validate.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"slices"
+	"strings"
 )
 
 func (cfg *apiConfig) validateChirpHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,20 +17,42 @@ func (cfg *apiConfig) validateChirpHandler(w http.ResponseWriter, r *http.Reques
 	err := json.NewDecoder(r.Body).Decode(&in)
 	if err != nil {
 		log.Printf("Error decoding JSON: %s", err)
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(500)
-		w.Write([]byte(`{"error": "Something went wrong"}`))
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
 		return
 	}
 
 	if len(in.Body) > 140 {
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(400)
-		w.Write([]byte(`{"error": "Chirp is too long"}`))
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
 		return
 	}
 
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write([]byte(`{"valid": true}`))
+	cleanedBody := replaceProfaneWords(in.Body)
+
+	response := map[string]string{"cleaned_body": cleanedBody}
+	respondWithJSON(w, http.StatusOK, response)
+}
+
+func replaceProfaneWords(body string) string {
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+	replacement := "****"
+
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		if slices.Contains(profaneWords, strings.ToLower(word)) {
+			words[i] = replacement
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
