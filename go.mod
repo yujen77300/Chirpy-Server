001f3bcbeb03fb0bd23a025e92c3b8a211f27497
yujen77300 <yujenhuang24@gmail.com>
2025-03-14 23:55:42 +0800

    feat: make JWT

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yujen77300/Chirpy-Server
 go 1.24.0
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,15 +1,73 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
-	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return "", err
-	}
-	return string(hashedBytes), nil
+    hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    if err != nil {
+        return "", err
+    }
+    return string(hashedBytes), nil
 }
 
 func CheckPasswordHash(password, hash string) error {
-	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+    claims := jwt.RegisteredClaims{
+        Issuer:    "chirpy",
+        IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+        ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+        Subject:   userID.String(),
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+    tokenString, err := token.SignedString([]byte(tokenSecret))
+    if err != nil {
+        return "", err
+    }
+
+    return tokenString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+    token, err := jwt.ParseWithClaims(
+        tokenString,
+        &jwt.RegisteredClaims{},
+        func(token *jwt.Token) (interface{}, error) {
+            if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+                return nil, errors.New("unexpected signing method")
+            }
+            return []byte(tokenSecret), nil
+        },
+    )
+
+    if err != nil {
+        return uuid.Nil, err
+    }
+
+    if !token.Valid {
+        return uuid.Nil, errors.New("invalid token")
+    }
+
+    claims, ok := token.Claims.(*jwt.RegisteredClaims)
+    if !ok {
+        return uuid.Nil, errors.New("invalid claims format")
+    }
+
+    userID, err := uuid.Parse(claims.Subject)
+    if err != nil {
+        return uuid.Nil, errors.New("invalid user ID in token")
+    }
+
+    return userID, nil
 }
